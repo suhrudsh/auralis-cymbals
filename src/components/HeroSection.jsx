1a230b6b0263698145e61639c4c04585951d32a9
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2025-11-21 14:53:25 +0530

    feat: Enhance HeroSection with GSAP animations for heading and subheading

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,50 @@
-import { Environment, OrbitControls } from "@react-three/drei";
+import { Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Lights } from "./Lights";
 import { HeroSectionScene } from "./HeroSectionScene";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(SplitText);
 
 export function HeroSection() {
+  const headingRef = useRef(null);
+  const subheadingRef = useRef(null);
+
+  useGSAP(() => {
+    const split = new SplitText(headingRef.current, {
+      type: "words",
+    });
+
+    const tl = gsap.timeline();
+
+    tl.fromTo(
+      split.words,
+      { filter: "blur(12px)", scale: 0.9, opacity: 0 },
+      {
+        filter: "blur(0px)",
+        scale: 1,
+        opacity: 1,
+        duration: 0.5,
+        stagger: 0.075,
+        ease: "power2.out",
+      },
+    ).fromTo(
+      subheadingRef.current,
+      { filter: "blur(12px)", scale: 0.9, opacity: 0 },
+      {
+        filter: "blur(0px)",
+        scale: 1,
+        opacity: 1,
+        duration: 0.75,
+        ease: "power2.out",
+      },
+      "-=0.5",
+    );
+  }, []);
+
   return (
     <section className="relative h-[95svh]">
       <div className="absolute -z-10 h-full w-full">
@@ -15,15 +56,14 @@ export function HeroSection() {
           <Lights />
           <Environment preset="forest" environmentIntensity={0.05} />
           <HeroSectionScene />
-          {/* <OrbitControls /> */}
         </Canvas>
       </div>
       <div className="to-bg flex h-full w-full flex-col items-center justify-center gap-8 bg-linear-to-b from-transparent from-60% text-center">
-        <h1 className="font-heading text-6xl font-black">
+        <h1 ref={headingRef} className="font-heading text-6xl font-black">
           Cymbals shaped by hand,
           <br /> built for sound.
         </h1>
-        <p className="font-body text-3xl leading-11 font-bold">
+        <p ref={subheadingRef} className="text-3xl leading-11 font-bold">
           Auralis is a collection of hand-crafted cymbals made to <br /> sound
           as unique as the drummers who play them.
         </p>
